2d4251468d09eef4ce21104d59539a2957b8ec0d
robbridges <robbridges@users.noreply.github.com>
2021-09-22 20:30:00 -0700

    Fixed the typescript a bit in all projects

--- weather-app/utils/weather.ts
+++ weather-app/utils/weather.ts
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
 const getWeatherData = async (location = 'yakima') => { 
   try {
-    const {data} = await axios.get(`http://api.weatherstack.com/current?access_key=e61d54f37409906fc4a83d8b8e27ee76&query=${location}&units=f`);
+    const {data} : AxiosRequestConfig = await axios.get(`http://api.weatherstack.com/current?access_key=e61d54f37409906fc4a83d8b8e27ee76&query=${location}&units=f`);
     
     return data;
   } catch (e) {
