50e7e7b1c1a263c556f920c1cf9110191400aed4
robbridges <robbridges@users.noreply.github.com>
2021-09-23 19:24:00 -0700

    Basic templating examples, been a while since I made any non static site, but good practice

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,15 +4,25 @@ var path = require("path");
 var express = require("express");
 var publicDir = path.join(__dirname, '../public');
 var app = express();
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'hbs');
 app.use(express.static(publicDir));
-app.get('/help', function (req, res) {
-    res.send({
-        name: 'Rob',
-        age: '33'
+app.get('', function (req, res) {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Rob Bridges'
     });
 });
 app.get('/about', function (req, res) {
-    res.send("<h1>\n  About us\n  </h1>");
+    res.render('about', {
+        title: 'About us'
+    });
+});
+app.get('/help', function (req, res) {
+    res.render('help', {
+        title: 'Come here if you are seeking help',
+        message: 'Yeah this is an unfinished page. No help to find here'
+    });
 });
 app.get('/weather', function (req, res) {
     res.send({
--- web-server/src/app.ts
+++ web-server/src/app.ts
@@ -1,27 +1,37 @@
 import path = require('path');
 import express = require('express');
 
+
 const publicDir = path.join(__dirname, '../public');
 
 
 const app = express();
+
+app.set('views', path.join(__dirname, '../views'));
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDir))
 
 
-
-app.get('/help', (req, res) => {
-  res.send({
-    name: 'Rob',
-    age: '33'
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Rob Bridges'
   });
-})
+});
 
 app.get('/about', (req, res) => {
-  res.send(
-  `<h1>
-  About us
-  </h1>`);
-})
+  res.render('about', {
+    title: 'About us',
+  });
+});
+
+app.get('/help', (req,res) => {
+  res.render('help', {
+    title: 'Come here if you are seeking help',
+    message: 'Yeah this is an unfinished page. No help to find here'
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
