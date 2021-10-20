0ba3353ed8cc11bc216fb0628a3db18f0a1b7d8b
robbridges <robbridges@users.noreply.github.com>
2021-10-20 09:34:34 -0700

    Bit more work

--- task-manager/js-output/src/emails/account.js
+++ task-manager/js-output/src/emails/account.js
@@ -3,7 +3,7 @@ const sgMail = require('@sendgrid/mail');
 const sendGridAPIkey = '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';
 sgMail.setApiKey(sendGridAPIkey);
 sgMail.send({
-    to: 'admin@ticketing-app-production.com',
+    to: 'Rmb101187@gmail.com',
     from: 'Rmb101187@gmail.com',
     subject: 'Test email',
     text: 'Just a test!',
