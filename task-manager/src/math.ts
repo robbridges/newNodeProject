249e91d456fd37f013c912fd64581449d5a60f7b
robbridges <robbridges@users.noreply.github.com>
2021-10-27 21:09:42 -0700

    Cleaning up testing now that I properly configured Jest

--- task-manager/src/math.ts
+++ task-manager/src/math.ts
@@ -1,5 +1,6 @@
-const calculateTip = (total: number, percent: number) => {
+const calculateTip = (total: number, percent: number): number=> {
   const tip = total * percent;
+  return total + tip;
 }
 
 export default calculateTip;
--- task-manager/tests/math.test.ts
+++ task-manager/tests/math.test.ts
@@ -2,6 +2,6 @@ import calculateTip from '../src/math'
 
 test('calculate tip', () =>  {
   const total = calculateTip(10, .3);
+  expect(total).toBe(13);
   
-  return total;
 })
