42875e933e0991916cb85dedfade309715b4814d
robbridges <robbridges@users.noreply.github.com>
2021-10-27 20:54:14 -0700

    Updated jest testing framework to work with typescript

--- task-manager/tests/math.test.ts
+++ task-manager/tests/math.test.ts
@@ -2,8 +2,6 @@ import calculateTip from '../src/math'
 
 test('calculate tip', () =>  {
   const total = calculateTip(10, .3);
-  if (total !== 13) {
-    throw new Error(`Total tip should be 13. We got ${total}`)
-  }
+  
   return total;
 })
